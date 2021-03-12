3e7da1188e3b65e244ba3ba488cb8104e6b618d5
MarcoValeri <marcovaleri@hotmail.it>
2021-03-12 18:24:05 +0000

    Add new super class

---
+++ SP1List.java
@@ -0,0 +1,25 @@
+public class SP1List {
+    // Instance variables
+    private Object[] items;
+    private int size;
+
+    // Constructors
+    public SP1List(int capacity) {
+        
+    };
+
+    public SP1List(Object[] array) {};
+
+    // Mutators
+    public boolean add(Object o) {};
+    public boolean set(Object o, int index) {};
+    public boolean insert(Object o, int index) {};
+    public Object remove(int index) {};
+
+    // Accessorts
+    public Object get(int index) {};
+    public int size() {};
+    public boolean contains(Object o) {};
+    public String toString() {};
+    public Object[] toArray() {};
+}
